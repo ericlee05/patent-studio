ba21b43dcdd103fc622a04e3a8ac88503e2cd8fa
VINTO1819 <develretr0@gmail.com>
2022-08-13 16:19:18 +0900

    날짜 유틸 기능추가

--- src/main/java/com/ericlee/pstudio/alpha/global/utils/DateUtil.java
+++ src/main/java/com/ericlee/pstudio/alpha/global/utils/DateUtil.java
@@ -2,7 +2,17 @@ package com.ericlee.pstudio.alpha.global.utils;
 
 import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+
 @Component
 public class DateUtil {
 
+    private final SimpleDateFormat YEAR_MONTH_DATE = new SimpleDateFormat("yyyy-MM-dd");
+    public String yearMonthDate(LocalDateTime lastModified) {
+        if(lastModified == null) {
+            return "-";
+        }
+        return YEAR_MONTH_DATE.format(lastModified);
+    }
 }
