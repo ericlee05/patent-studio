49793ffbf0dbfc4ecf6b6700a21588b3fe48ac41
VINTO1819 <develretr0@gmail.com>
2022-08-14 20:28:38 +0900

    검증 AOP 적용

--- src/main/java/com/ericlee/pstudio/alpha/domain/patent/service/PatentEditorService.java
+++ src/main/java/com/ericlee/pstudio/alpha/domain/patent/service/PatentEditorService.java
@@ -1,16 +1,20 @@
 package com.ericlee.pstudio.alpha.domain.patent.service;
 
 import com.ericlee.pstudio.alpha.domain.patent.entity.Patent;
-import com.ericlee.pstudio.alpha.domain.patent.exception.PatentAccessDeniedException;
+import com.ericlee.pstudio.alpha.domain.patent.exception.PatentComponentNotFoundException;
 import com.ericlee.pstudio.alpha.domain.patent.exception.PatentNotFoundException;
 import com.ericlee.pstudio.alpha.domain.patent.repository.PatentRepository;
+import com.ericlee.pstudio.alpha.domain.patent.type.SingleComponentType;
 import com.ericlee.pstudio.alpha.domain.user.entity.User;
 import com.ericlee.pstudio.alpha.domain.user.exception.UnauthorizedUserException;
 import com.ericlee.pstudio.alpha.domain.user.facade.UserFacade;
+import com.ericlee.pstudio.alpha.global.aop.ValidPatentAccessAspect;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+
+import java.util.Arrays;
 
 @RequiredArgsConstructor
 @Service
@@ -18,11 +22,9 @@ public class PatentEditorService {
     private final PatentRepository patentRepository;
     private final UserFacade userFacade;
 
+    @ValidPatentAccessAspect.ValidPatentAccess
     @Transactional(readOnly = true)
-    public void loadEditor(Long patentId, Model model) {
-        User actionUser = userFacade.queryCurrentUser(true)
-                .orElseThrow(UnauthorizedUserException::new);
-
+    public void loadEditor(@ValidPatentAccessAspect.PatentId Long patentId, Model model) {
         Patent patent = patentRepository.findById(patentId)
                 .orElseThrow(PatentNotFoundException::new);
 
