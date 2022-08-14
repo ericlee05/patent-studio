5bedc613d05e47f703937b46b787764c3af6d0d1
VINTO1819 <develretr0@gmail.com>
2022-08-14 20:31:12 +0900

    특허 단일 구성요소 편집기능 구현

--- src/main/java/com/ericlee/pstudio/alpha/domain/patent/presentation/EditorController.java
+++ src/main/java/com/ericlee/pstudio/alpha/domain/patent/presentation/EditorController.java
@@ -1,10 +1,21 @@
 package com.ericlee.pstudio.alpha.domain.patent.presentation;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ericlee.pstudio.alpha.domain.patent.service.PatentEditorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RequestMapping("/edit-patent")
 @RestController
 public class EditorController {
+    private final PatentEditorService editorService;
 
+    @GetMapping("/{patent-id}/single/{type-ordinal}")
+    public String getSingleComponent(@PathVariable("patent-id") Long patentId, @PathVariable("type-ordinal") Integer typeOrdinal) {
+        return editorService.getSingleComponent(patentId, typeOrdinal);
+    }
+    @PostMapping("/{patent-id}/single/{type-ordinal}")
+    public void updateSingleComponent(@PathVariable("patent-id") Long patentId, @PathVariable("type-ordinal") Integer typeOrdinal, @RequestBody String jsonBody) {
+        editorService.updateSingleComponent(patentId, typeOrdinal, jsonBody);
+    }
 }
