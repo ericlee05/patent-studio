0b9aff57a206636f29f01f9a9b03fc15f8104480
VINTO1819 <develretr0@gmail.com>
2022-08-14 20:27:49 +0900

    특허 생성 시 단일 구성요소 자동생성

--- src/main/java/com/ericlee/pstudio/alpha/domain/patent/service/PatentSimpleService.java
+++ src/main/java/com/ericlee/pstudio/alpha/domain/patent/service/PatentSimpleService.java
@@ -2,10 +2,13 @@ package com.ericlee.pstudio.alpha.domain.patent.service;
 
 import com.ericlee.pstudio.alpha.domain.patent.entity.Patent;
 import com.ericlee.pstudio.alpha.domain.patent.entity.PatentDetail;
+import com.ericlee.pstudio.alpha.domain.patent.entity.SingleComponent;
+import com.ericlee.pstudio.alpha.domain.patent.entity.SingleComponentId;
 import com.ericlee.pstudio.alpha.domain.patent.exception.PatentAccessDeniedException;
 import com.ericlee.pstudio.alpha.domain.patent.exception.PatentNotFoundException;
 import com.ericlee.pstudio.alpha.domain.patent.presentation.dto.request.PatentCreationRequest;
 import com.ericlee.pstudio.alpha.domain.patent.repository.PatentRepository;
+import com.ericlee.pstudio.alpha.domain.patent.type.SingleComponentType;
 import com.ericlee.pstudio.alpha.domain.user.entity.User;
 import com.ericlee.pstudio.alpha.domain.user.exception.UnauthorizedUserException;
 import com.ericlee.pstudio.alpha.domain.user.facade.UserFacade;
@@ -17,6 +20,9 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -24,6 +30,15 @@ public class PatentSimpleService {
     private final PatentRepository patentRepository;
     private final UserFacade userFacade;
     private final HttpUtil httpUtil;
+
+    private List<SingleComponent> getNecessaryComponents(Patent patent) {
+        return Arrays.stream(SingleComponentType.values())
+                .map(type -> SingleComponent.builder()
+                        .id(SingleComponentId.builder().singleComponentType(type).patent(patent).build())
+                        .content(String.format("%s에 대해 작성해주세요", type.getDescription()))
+                        .build())
+                .collect(Collectors.toList());
+    }
 
     @Transactional
     public void createPatent(PatentCreationRequest request, HttpServletResponse response) {
@@ -38,10 +53,11 @@ public class PatentSimpleService {
                 .drawings(new ArrayList<>())
                 .singleComponents(new ArrayList<>())
                 .multiComponents(new ArrayList<>())
-                .claims(new ArrayList<>(new ArrayList<>()))
+                .claims(new ArrayList<>())
                 .organization(user.getOrganization())
                 .build();
         patent = patentRepository.save(patent);
+        patent.getSingleComponents().addAll(getNecessaryComponents(patent));
         user.getOrganization().addPatent(patent);
 
         httpUtil.redirectTo(response, String.format("/patents/%s", patent.getId()));
