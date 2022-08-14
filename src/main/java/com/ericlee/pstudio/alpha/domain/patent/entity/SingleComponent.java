f523f5e9487de55857a3072fe4ba53ee9cc22ec4
VINTO1819 <develretr0@gmail.com>
2022-08-14 20:27:02 +0900

    단일 구성요소에 수정기능 추가

--- src/main/java/com/ericlee/pstudio/alpha/domain/patent/entity/SingleComponent.java
+++ src/main/java/com/ericlee/pstudio/alpha/domain/patent/entity/SingleComponent.java
@@ -1,5 +1,6 @@
 package com.ericlee.pstudio.alpha.domain.patent.entity;
 
+import com.ericlee.pstudio.alpha.domain.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,6 +10,7 @@ import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Lob;
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -22,4 +24,9 @@ public class SingleComponent {
     @Column(nullable = false)
     @Lob
     private String content;
+    public void update(User updateUser, String content) {
+        id.getPatent().getDetail().setLastModifier(updateUser);
+        id.getPatent().getDetail().setLastModified(LocalDateTime.now());
+        this.content = content;
+    }
 }
